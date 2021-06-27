e3bcf1bb631bce9551a457d16d9092904e8083f8
adrianz261 <adrian.zuchowski@outlook.com>
2021-06-27 10:35:33 +0100

    Fixed sortByPesel() method. It works properly now.

--- UniversityDB.cpp
+++ UniversityDB.cpp
@@ -12,57 +12,51 @@ University::University(const std::vector<Student> student_list)
 {
 }
 
-void University::addStudent(const Student& student)
+void University::addStudent(const Student &student)
 {
     student_list_.push_back(student);
 }
 
-Student University::searchBySurname(const std::string& surname)
+Student University::searchBySurname(const std::string &surname)
 {
     return *std::find_if(student_list_.cbegin(), student_list_.cend(),
-                        [surname](const auto& s)
-                        { return s.getSurname() == surname; });
+                         [surname](const auto &s)
+                         { return s.getSurname() == surname; });
 }
 
-Student University::searchByPesel(const std::string& pesel)
+Student University::searchByPesel(const std::string &pesel)
 {
     return *std::find_if(student_list_.cbegin(), student_list_.cend(),
-                    [pesel](const Student& s)
-                    {
-                        return s.getPesel() == pesel;
-                    });
+                         [pesel](const Student &s)
+                         {
+                             return s.getPesel() == pesel;
+                         });
 }
 
 void University::sortByPesel()
 {
     std::sort(student_list_.begin(), student_list_.end(),
-                [](const Student& lhs, const Student& rhs)
-                {
-                    const std::string& lhs_pesel = lhs.getPesel();
-                    const std::string& rhs_pesel = rhs.getPesel();
+              [](const Student &lhs, const Student &rhs)
+              {
+                  const std::string &lhs_pesel = lhs.getPesel();
+                  const std::string &rhs_pesel = rhs.getPesel();
 
-                    for (size_t i = 0; i < 11; ++i)
-                    {
-                        //std::cerr << i << ". " << lhs_pesel[i] << " | " << rhs_pesel[i] << '\n';
-                        if (lhs_pesel[i] < rhs_pesel[i])
-                        {
-                            return true;
-                        }
-                    }
-                    return false;
-                });
+                  return lhs_pesel < rhs_pesel;
+              });
 }
 
 void University::sortBySurname()
 {
     std::sort(student_list_.begin(), student_list_.end(),
-                [](const Student& lhs, const Student& rhs) { return lhs.getSurname() < rhs.getSurname(); });
+              [](const Student &lhs, const Student &rhs)
+              { return lhs.getSurname() < rhs.getSurname(); });
 }
 
 bool University::removeStudent(const unsigned long int index_number)
 {
     auto it = std::remove_if(student_list_.begin(), student_list_.end(),
-                    [index_number](const Student& s) { return s.getIndexNumber() == index_number; });
+                             [index_number](const Student &s)
+                             { return s.getIndexNumber() == index_number; });
     if (it == student_list_.end())
     {
         return false;
@@ -75,15 +69,15 @@ bool University::removeStudent(const unsigned long int index_number)
 void University::printStudents() const
 {
     std::cout << "*********************************************\n";
-    for (const auto& student : student_list_)
+    for (const auto &student : student_list_)
     {
         std::cout << "* " << student.getName()
-                << " | " << student.getSurname()
-                << " | " << student.getAddress()
-                << " | " << student.getIndexNumber()
-                << " | " << student.getGender()
-                << " | " << student.getPesel()
-                << " *\n";
+                  << " | " << student.getSurname()
+                  << " | " << student.getAddress()
+                  << " | " << student.getIndexNumber()
+                  << " | " << student.getGender()
+                  << " | " << student.getPesel()
+                  << " *\n";
     }
     std::cout << "**********************************************\n";
 }
